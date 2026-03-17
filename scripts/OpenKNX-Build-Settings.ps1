4e674f7025f650b1c360417daa5310319bce1120
Matthias <m.freudenreich@smart-mf.de>
2026-03-17 22:25:20 +0100

    fix Release Name

--- scripts/OpenKNX-Build-Settings.ps1
+++ scripts/OpenKNX-Build-Settings.ps1
@@ -1,5 +1,5 @@
 # set product names, allows mapping of (devel) name in Project to a more consistent name in release
-$settings = lib/OGM-Common/scripts/build/OpenKNX-Build-Settings.ps1 $args[0] "ModbusModule" "ModbusModule"
+$settings = lib/OGM-Common/scripts/build/OpenKNX-Build-Settings.ps1 $args[0] "ModbusModule" "ModbusGateway"
 
 $settings.releaseName = "ModbusGateway"
 
